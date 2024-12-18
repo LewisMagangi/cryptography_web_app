4ed3d578e59206942ab7704f2ebd66e10179aba3
Liquelaliqour <lewismomanyi34@gmail.com>
2024-12-18 13:35:57 +0300

    Fake Content Input to simulate database

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+# Creating Views
 
-# C
+from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author':'Lewis',
+        'title':'Blog Post 1',
+        'content':'This is the first post.',
+        'date_posted':'2024-11-12'
+    },
+    {
+        'author':'Mark',
+        'title':'Blog Post 2',
+        'content':'This is the second post.',
+        'date_posted':'2024-11-12'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
