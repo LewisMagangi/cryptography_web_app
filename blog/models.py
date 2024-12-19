5ed9f2f32ce3d2e3fcc34b3a12e8807f04d39fa4
Liquelaliqour <lewismomanyi34@gmail.com>
2024-12-19 11:00:01 +0300

    Creating a Post model.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# Create your models hear
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=1000)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
