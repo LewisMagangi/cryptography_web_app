75c533aebaa15443f0f419b989f50a22b33162da
Liquelaliqour <lewismomanyi34@gmail.com>
2024-12-19 13:49:34 +0300

    Adding users app in Installed Apps, adding register path, Validating the form submitted by the register page.

--- cryptography_web_app/settings.py
+++ cryptography_web_app/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'users.app.UserConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
