b358489d2dd38af9f0ea75146b4eec652761e630
Liquelaliqour <lewismomanyi34@gmail.com>
2024-12-26 15:35:16 +0300

    Template to confirm post deletion

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 def home(request):
     context = {
@@ -38,6 +38,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
 
     def test_func(self):
         post = self.get_object()
