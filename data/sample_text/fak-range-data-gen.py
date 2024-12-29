b84d5506a62fa4c43b25983cd798be76bec56031
Liquelaliqour <lewismomanyi34@gmail.com>
2024-12-29 22:57:12 +0300

    A script to generate sized data. NB The generated files aren't pushed but will be downloaded individually.

---
+++ data/sample_text/fak-range-data-gen.py
@@ -0,0 +1,23 @@
+import os
+from faker import Faker
+
+def generate_text_data(size_in_mb):
+    text = ''
+    size_in_bytes = size_in_mb * 1024 * 1024  # Convert MB to bytes
+    text_generator = Faker()
+    while len(text.encode('utf-8')) < size_in_bytes:
+        remaining_bytes = size_in_bytes - len(text.encode('utf-8'))
+        chunk_size = min(remaining_bytes, 1024)  # Chunk size of 1024 bytes or remaining bytes, whichever is smaller
+        text += text_generator.text()
+    return text
+
+# Sizes to generate text data for
+sizes = [1, 2, 5, 10, 20, 50, 100]
+
+# Generate text data for each size and save it into a separate file
+for size in sizes:
+    text_data = generate_text_data(size)
+    file_name = f"{size}mb_text_data_faker.txt"
+    with open(file_name, 'w') as file:
+        file.write(text_data)
+    print(f"Generated {size}MB of text data using faker and saved it into {file_name}")
