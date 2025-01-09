c989867ec4078b0ae7e55693f079a3cb234437e4
Liquelaliqour <lewismomanyi34@gmail.com>
2025-01-09 19:33:02 +0300

    Included a redirect url upon successful update or delete form submission

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 # Creating Views
 
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 
 def home(request):
@@ -41,7 +42,10 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
+    def get_success_url(self):
+        return reverse_lazy('blog-home')
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
@@ -56,9 +60,12 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+    def get_success_url(self):
+        return reverse_lazy('blog-home')
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url = '/'
+    success_url = reverse_lazy('blog-home')
 
     def test_func(self):
         post = self.get_object()
