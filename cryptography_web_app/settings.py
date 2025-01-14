9de21bf71a5f0cf32fe3ee994618c301e40a0d01
LewisMagangi <lewismomanyi34@gmail.com>
2025-01-14 14:51:35 +0000

    Allowing my ip address in the settings.py file, creating a static folder and collecting the static files to the respective folder.

--- cryptography_web_app/settings.py
+++ cryptography_web_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wj3gn$qx%88)9fs7vkqq&&%+_5imx4imsvf-s3knd5teadonyu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.234.99.63']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
