1117ba4bb183e17b7ce053b0271759cff415b041
Liquelaliqour <lewismomanyi34@gmail.com>
2025-01-22 18:57:45 +0300

    Updating the Post Model and it's respective view

--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'chart_image']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -48,7 +48,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'chart_image']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
